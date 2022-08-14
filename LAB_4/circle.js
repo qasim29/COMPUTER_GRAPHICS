68f95862b57c19b34afb0bc8c831c10dc7e488de
qasim.29 <qasimmohammad534@gmail.com>
2022-08-14 17:11:40 +0500

    circle.js is working now.

--- LAB_4/circle.js
+++ LAB_4/circle.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 200);
-camera.position.set(0, 0, 100);
+camera.position.set(0, 0, 3);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -12,7 +12,7 @@ const geometry = new THREE.BufferGeometry();
 
 let point=[];
 
-const angle=2*Math.PI;
+const angle=2*Math.PI/40;
 
 for(let i=0; i<=40; i++){
     point.push(new THREE.Vector3(Math.cos(angle*i),Math.sin(angle*i),0));
@@ -26,9 +26,26 @@ const material = new THREE.LineBasicMaterial({
 
 let circle = new THREE.Line(geometry, material);
 
+scene.add(circle);
+
 //end circle 
 
 
+function animate() {    
+    circle.rotation.x += 0.01;
+    renderer.render(scene,camera);
+    requestAnimationFrame(animate);
+}
 
-scene.add(circle);
-renderer.render(scene,camera);
+animate()
+
+
+// make window responsive to resize evernts
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect=window.innerWidth/window.innerHeight;
+   
+
+});
+
+
