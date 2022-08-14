246dd6387becde9f8cc5844c313ca51b576d9d8f
qasim.29 <qasimmohammad534@gmail.com>
2022-08-14 05:53:39 +0500

    Improved packages organization

--- LAB_3/rain.js
+++ LAB_3/rain.js
@@ -106,7 +106,7 @@ animate();
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth,window.innerHeight);
     camera.aspect=window.innerWidth/window.innerHeight;
-    camera.updateProjectMatrix();
+   
 
 })
 
