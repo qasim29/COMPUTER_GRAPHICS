1d9d358ba9f895d32ca0b5968f8cd9d01de10ca6
qasim.29 <qasimmohammad534@gmail.com>
2022-08-17 22:07:09 +0500

    completed pentagon

---
+++ LAB_3/pentagon.js
@@ -0,0 +1,46 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 200);
+camera.position.set(0, 0, 3);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+// START Helix
+const geometry = new THREE.BufferGeometry();
+
+let point=[];
+
+const angle=2*Math.PI/5;
+
+for(let i=0; i<=5; i++){
+    point.push(new THREE.Vector3(Math.cos(angle*i),Math.sin(angle*i),0));    
+}
+geometry.setFromPoints(point);
+
+const material = new THREE.LineBasicMaterial({ 
+    color: "rgb(149, 199, 232)", 
+});
+
+let circle = new THREE.Line(geometry, material);
+
+scene.add(circle);
+
+//end circle
+function animate() {    
+    circle.rotation.x += 0.01;
+    renderer.render(scene,camera);
+    requestAnimationFrame(animate);
+}
+
+animate()
+
+
+// make window responsive to resize evernts
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect=window.innerWidth/window.innerHeight;
+   
+
+});
